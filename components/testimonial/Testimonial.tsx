15da3240567e6bcc8e9508ed55a8d43299f254c4
Ece Tunca <ecetunca@Eces-MacBook-Pro.local>
2020-06-07 00:29:03 +0200

    testimonial

---
+++ components/testimonial/Testimonial.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from 'react'
+import classNames from 'classnames'
+import QuoteSign from './images/quote.svg'
+
+type Props = {
+  theme?: 'fancy' | 'simple'
+  quote: string
+  name?: string
+  position?: string
+  imageUrl?: string
+}
+
+const Testimonial: FC<Props> = ({
+  theme = 'simple',
+  quote,
+  name,
+  position,
+  imageUrl,
+}) => {
+  const blockClass = classNames('testimonial', {
+    'testimonial--simple': theme === 'simple',
+    'testimonial--fancy': theme === 'fancy',
+  })
+
+  return (
+    <div className={blockClass}>
+      {imageUrl && (
+        <div className="testimonial__image">
+          <img src={imageUrl} />
+        </div>
+      )}
+      <div className="testimonial__container">
+        <blockquote className="testimonial__quote">
+          {theme === 'fancy' && (
+            <div className="testimonial__quote-sign">
+              <QuoteSign />
+            </div>
+          )}
+          {quote}
+        </blockquote>
+        {name && (
+          <div className="testimonial__person">
+            <span className="testimonial__name">{name}</span>
+            {position && (
+              <div className="testimonial__position-container">
+                {theme === 'fancy' && (
+                  <span className="testimonial__slash">/</span>
+                )}
+                <span className="testimonial__position">{position}</span>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Testimonial
---
+++ pages/components/testimonial.tsx
@@ -0,0 +1,66 @@
+import Head from 'next/head'
+import Testimonial from 'components/testimonial/Testimonial'
+
+export default function TestimonialPage() {
+  return (
+    <div className="site">
+      <Head>
+        <title>Leila UI</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+
+      <main>
+        <h1 className="site__title">Welcome to Leila UI</h1>
+        <section className="section">
+          <h2 className="section__title">Testimonial Fancy</h2>
+          <div className="section__content">
+            <Testimonial
+              theme="fancy"
+              quote="Nap all day cat dog hate mouse eat string barf pillow no baths hate everything but kitty poochy. Sleep on keyboard toy mouse squeak roll over. Mesmerizing birds. Poop on grasses licks paws destroy couch intently sniff hand. The dog smells bad gnaw the corn cob."
+              name="Leila Whitepanther"
+              position="Director of Naps"
+            />
+          </div>
+        </section>
+        <section className="section">
+          <h2 className="section__title">Testimonial Simple</h2>
+          <div className="section__content">
+            <Testimonial
+              quote="Nap all day cat dog hate mouse eat string barf pillow no baths hate everything but kitty poochy. Sleep on keyboard toy mouse squeak roll over. Mesmerizing birds. Poop on grasses licks paws destroy couch intently sniff hand. The dog smells bad gnaw the corn cob."
+              name="Leila Whitepanther"
+              position="Director of Naps"
+            />
+          </div>
+        </section>
+        <section className="section">
+          <h2 className="section__title">Testimonial Image</h2>
+          <div className="section__content">
+            <Testimonial
+              theme="fancy"
+              quote="Nap all day cat dog hate mouse eat string barf pillow no baths hate everything but kitty poochy. Sleep on keyboard toy mouse squeak roll over. Mesmerizing birds. Poop on grasses licks paws destroy couch intently sniff hand. The dog smells bad gnaw the corn cob."
+              name="Leila Whitepanther"
+              position="Director of Naps"
+              imageUrl="https://www.natureplprints.com/p/729/blue-eyed-tabby-white-siberian-cross-kitten-15336697.jpg"
+            />
+          </div>
+          <div className="section__content">
+            <Testimonial
+              quote="Nap all day cat dog hate mouse eat string barf pillow no baths hate everything but kitty poochy. Sleep on keyboard toy mouse squeak roll over. Mesmerizing birds. Poop on grasses licks paws destroy couch intently sniff hand. The dog smells bad gnaw the corn cob."
+              name="Leila Whitepanther"
+              position="Director of Naps"
+              imageUrl="https://www.natureplprints.com/p/729/blue-eyed-tabby-white-siberian-cross-kitten-15336697.jpg"
+            />
+          </div>
+        </section>
+      </main>
+    </div>
+  )
+}
